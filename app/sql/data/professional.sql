5013dc5f9de2b84c6d38f4319909ad3fcaea493d
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2023-11-08 16:02:45 -0500

    ✨ [UPDATE] schemas

---
+++ app/sql/data/professional.sql
@@ -0,0 +1,12 @@
+INSERT INTO professional_positions
+(employer_id, user_id, job_title, start_year, end_year)
+VALUES
+(1, 1, 'Business Analyst', 2000, 2004),
+(2, 1, 'Senior economist', 2004, 2010),
+(3, 3, 'Researcher', 2005, NULL),
+(4, 2, 'Researcher', 2010, 2014),
+(4, 2, 'Researcher Lead', 2014, NULL),
+(5, 3, 'Frontend Developer', 2011, 2014),
+(5, 4, 'Frontend Developer', 2014, NULL),
+(6, 5, 'Backend Developer', 2008, NULL),
+(6, 6, 'Designer', 2005, NULL);
