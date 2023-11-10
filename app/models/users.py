2c175ddc5c4568c65e1abec37da51aea9a88485b
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2023-11-10 10:28:21 -0500

    ✨ [UPDATE] models

--- app/models/users.py
+++ app/models/users.py
@@ -1,23 +1,48 @@
 from typing import List
 
 # sqlaclhemy
-from sqlalchemy import String
+from sqlalchemy import ForeignKey, String
 # orm
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 # models
 from app.common.models.base import CustomBaseModel
+#schemas
+from app.schemas.users import Profile as ProfileSchema
+from app.schemas.users import User as UserSchema
 
 
 class User(CustomBaseModel):
     __tablename__ = "users"
+    schema = UserSchema
+    validate = True
+
+    #
     email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False, index=True)
     password: Mapped[str] = mapped_column(String(255), nullable=False)
     educations: Mapped[List["Education"]] = relationship()
     professional_positions: Mapped[List["ProfessionalPosition"]] = relationship()
+    profile: Mapped["Profile"] = relationship(back_populates="user")
 
     @classmethod
     def get_by_email(cls, email: str):
         with cls.db.session() as session:
             item = session.query(cls).filter_by(email=email).first()
+            item = cls._validate(item)
         return item
+
+
+class Profile(CustomBaseModel):
+    __tablename__ = "profiles"
+    schema = ProfileSchema
+    validate = True
+
+    #
+    first_name: Mapped[str] = mapped_column(String(128))
+    last_name: Mapped[str] = mapped_column(String(128))
+    sex: Mapped[str] = mapped_column(String(10))
+    current_city: Mapped[str] = mapped_column(String(255))
+    hometown: Mapped[str] = mapped_column(String(255))
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    user: Mapped["User"] = relationship(back_populates="profile")
--- app/schemas/users/__init__.py
+++ app/schemas/users/__init__.py
@@ -1,5 +1,28 @@
+from datetime import datetime
+from enum import Enum
+
+#
+from pydantic import Field
+
+#
 from app.schemas.base import CustomBaseSchema
 
 
 class User(CustomBaseSchema):
     email: str
+    password: str = Field(exclude=True)
+
+
+class ProfileSexEnum(str, Enum):
+    F: "F"
+    M: "M"
+    
+
+class Profile(CustomBaseSchema):
+    user_id: int = Field(ge=1, examples=[1])
+    sex: ProfileSexEnum
+    first_name: str = Field(...)
+    last_name: str = Field(...)
+    birthday: datetime
+    current_city: str = Field(...)
+    hometown: str = Field(...)
